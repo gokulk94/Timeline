0bdaa809af654daf58071b037ce5956fc4d3721f
Gokul K <gokulk@qburst.com>
2016-07-10 23:25:38 +0530

    Added react-router

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppContainer from './components/AppContainer.jsx';
+import { Router, Route, Link, browserHistory } from 'react-router';
 
-ReactDOM.render(<AppContainer />, document.getElementById('main'));
+ReactDOM.render((
+	<Router history={browserHistory}>
+		<Route path="/" component={AppContainer}>
+			<Route path="*" component={AppContainer} />
+		</Route>
+	</Router>
+	),document.getElementById('main'))
